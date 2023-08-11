e0896810adb0a670d2f4cdaba8ece8b0644035fa
CodeMode365 <codemode365@gmail.com>
2023-08-11 08:56:00 +0530

    Locaking image and content

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -19,9 +19,9 @@ export default function Error({
   return (
     <div className='w-full h-full bg-black/80 min-h-[92vh] flex items-center justify-center'>
       <div className='flex items-center justify-center flex-col max-w-[550px] text-center'>
-        <Image src={"/Lock.png"} alt='Error Image' height={150} width={150} className='mb-3'/>
-        <h2 className='text-2xl my-1 font-semibold text-white'>This content isn't available right now!</h2>
-        <p className='text-white/80 text-lg leading-6'>When this happens, it's usually beacause the owner only shared it with a small group of people, changed who can see it or it's been deleted.</p>
+        <Image src={"/lock.png"} alt='Error Image' height={150} width={150} className='mb-3'/>
+        <h2 className='text-2xl my-1 font-semibold text-white'>This content isn&apos;t available right now!</h2>
+        <p className='text-white/80 text-lg leading-6'>When this happens, it&apos;s usually beacause the owner only shared it with a small group of people, changed who can see it or it&apos;s been deleted.</p>
 
         <button className={` text-white rounded-md flex items-center bg-blue-500 hover:bg-blue-400 my-4 `}>
           <Link href={"/"} className='px-2 py-3 rounded-md'>
