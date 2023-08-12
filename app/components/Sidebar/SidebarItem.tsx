b57c6012a172f0c1b19609d9c775c5756f009f04
CodeMode365 <codemode365@gmail.com>
2023-08-12 19:22:51 +0530

    no modification

--- app/components/Sidebar/SidebarItem.tsx
+++ app/components/Sidebar/SidebarItem.tsx
@@ -10,9 +10,9 @@ interface SidebarItemProps {
 const SidebarItem: React.FC<SidebarItemProps> = ({ name, icon: Icon, active }) => {
 
     return (
-        <div key={name} className={`flex flex-row items-center cursor-pointer rounded-md text-lg font-bold my-1 text-white/80 hover:bg-white/10 ${active ? "bg-white/10" : ""} px-4 py-3`}>
-            <span className={`${active ? 'bg-blue-500' : 'bg-white/10'} rounded-full flex items-center justify-center p-2 cursor-pointer`}>
-                <Icon size={30} />
+        <div key={name} className={`flex flex-row items-center cursor-pointer rounded-md text-lg font-bold my-[1px] text-white/80 hover:bg-white/10 ${active ? "bg-white/10" : ""} px-4 py-3`}>
+            <span className={`${active ? 'bg-blue-500' : 'bg-white/10'} rounded-full flex items-center justify-center p-1 cursor-pointer`}>
+                <Icon size={28} />
             </span>
             <span className='mx-4'>{name}</span>
         </div>
--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -10,7 +10,7 @@ const Market = () => {
       <div className='w-11/12 mx-auto'>
         {/* titlebar */}
         <div className='w-full flex items-center justify-between'>
-          <h2 className='text-white text-3xl font-bold'>Today's Pick</h2>
+          <h2 className='text-white text-3xl font-bold'>{`Today's Pick`}</h2>
           <button className='text-blue-500 flex items-center'>
             <IoLocationSharp size={20} />
             <span className='mx-2'>
@@ -22,7 +22,7 @@ const Market = () => {
         <div className='w-full my-4 h-auto grid gap-x-4 gap-y-10 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 '>
           {
             Array.from({ length: 15}).map((item, ind) => (
-              <ShoppingCard title={'Drone camera'} price={'रू500'} location={'Mahangkal, Bagmati'} key={ind} />
+              <ShoppingCard title={'Drone camera'} price={'रू500'} location={'Mahangkal Bagmati'} key={ind} />
             ))
           }
         </div>
