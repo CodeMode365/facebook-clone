a3abff185edc70a8a1a67a10f6a1628f0829db1f
CodeMode365 <codemode365@gmail.com>
2023-08-12 19:23:55 +0530

    Profile sub component

---
+++ app/profile/components/FriendsBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FriendsBox = () => {
+  return (
+    <div>FriendsBox</div>
+  )
+}
+
+export default FriendsBox
---
+++ app/profile/reels/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const reels = () => {
+  return (
+    <div>reels</div>
+  )
+}
+
+export default reels
